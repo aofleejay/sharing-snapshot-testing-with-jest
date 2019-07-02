b811142c7a73ca84ceac6c596a51027d6d5298ae
Kunapot Pairat <aofleejay@hotmail.com>
2019-07-02 23:24:02 +0700

    :sparkles: Inline snapshot.

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App'
 
 describe('Test app component', () => {
   it('shallow snapshot', () => {
-    const renderer = new shallowRenderer
+    const renderer = new shallowRenderer()
     const snapshot = renderer.render(<App />)
     expect(snapshot).toMatchSnapshot()
   })
@@ -13,5 +13,35 @@ describe('Test app component', () => {
   it('full snapshot', () => {
     const html = renderer.create(<App />).toJSON()
     expect(html).toMatchSnapshot()
+  })
+
+  it('inline snapshot', () => {
+    const renderer = new shallowRenderer()
+    const snapshot = renderer.render(<App />)
+    expect(snapshot).toMatchInlineSnapshot(`
+      <section
+        className="hero is-primary"
+      >
+        <div
+          className="hero-body"
+        >
+          <div
+            className="container"
+          >
+            <h1
+              className="title"
+            >
+              React Snapshot Testing With Jest
+            </h1>
+            <h2
+              className="subtitle"
+            >
+              Kunapot Pairat
+            </h2>
+            <Link />
+          </div>
+        </div>
+      </section>
+    `)
   })
 })
